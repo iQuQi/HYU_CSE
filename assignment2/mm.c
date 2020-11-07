bfe1d15e2e865d48f82a9657da74e1b50c12ad7a
2019062833 <2019062833@hanyang.ac.kr>
2020-11-08 00:52:33 +0900

    re

--- assignment2/mm.c
+++ assignment2/mm.c
@@ -76,8 +76,8 @@ static void place(void *bp, size_t asize);
 #define sideby(size) (((size) + (eight-1)) & ~0x7)
 #define SIZE_T_SIZE (sideby(sizeof(size_t)))
 
-static char *_heap_listp = 0;
-static char **_free_listp = 0;
+static char *HeapBlocks = 0;
+static char **FreeBlocks = 0;
 static void free_list_insert(void* bp);
 static void free_list_remove(void* bp);
 
@@ -94,18 +94,18 @@ static void put(void *bp, unsigned int t);
 int mm_init(void)
 {
 	/* create the initial empty heap */
-	if ((_heap_listp = mem_sbrk(WSIZE*(ClassSize + 2 + 1))) == (void *)-1)
+	if ((HeapBlocks = mem_sbrk(WSIZE*(ClassSize + 2 + 1))) == (void *)-1)
 		return -1;
 
-	memset(_heap_listp, 0, ClassSize*WSIZE);
-	_free_listp = (char **)_heap_listp;
+	memset(HeapBlocks, 0, ClassSize*WSIZE);
+	FreeBlocks = (char **)HeapBlocks;
 
 
-	_heap_listp += ClassSize * WSIZE;
-	PUT(_heap_listp, PACK(DSIZE, 1));
-	PUT(_heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
-	PUT(_heap_listp + (2 * WSIZE), PACK(0, 1));
-	_heap_listp += (1 * WSIZE);
+	HeapBlocks += ClassSize * WSIZE;
+	PUT(HeapBlocks, PACK(DSIZE, 1));
+	PUT(HeapBlocks + (1 * WSIZE), PACK(DSIZE, 1));
+	PUT(HeapBlocks + (2 * WSIZE), PACK(0, 1));
+	HeapBlocks += (1 * WSIZE);
 	/* Extend the empty heap with a free block of CHUNKSIZE bytes */
 	if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
 		return -1;
@@ -122,30 +122,30 @@ void *mm_malloc(size_t size)
 {
 
 	char *bp;
-	size_t reSize;
-	size_t newSize, tmp_size;
+	size_t size2;
+	size_t newSize;
 
 	//만약 사이즈가 0이라면 에러처리로 널리턴
 	if (size <= 0)
 		return NULL;
 
 	//사이즈가 더블워드사이즈 보다 크다면 
-	if (size > DSIZE) reSize = DSIZE * ((size + DSIZE + (DSIZE - 1)) / DSIZE);
-	//작거나 같다면 reSize는 더블사이즈의 2배
-	else reSize = 2 * DSIZE;
+	if (size > DSIZE) size2 = DSIZE * ((size + DSIZE + (DSIZE - 1)) / DSIZE);
+	//작거나 같다면 size2는 더블사이즈의 2배
+	else size2 = 2 * DSIZE;
 
 	//프리리스트에서 들어갈 자리를 찾아서 넣는다.
-	if ((bp = find_fit(reSize)) != NULL) {
-		place(bp, reSize);
+	if ((bp = find_fit(size2)) != NULL) {
+		place(bp, size2);
 		return bp;
 	}
 
 	//들어갈 자리가 없다면 공간을 확장
-	newSize = MAX(reSize, CHUNKSIZE);
+	newSize = MAX(size2, CHUNKSIZE);
 	//확장이 실패했다면 null리턴
 	if ((bp = extend_heap(newSize / WSIZE)) == NULL) return NULL;
-	//아니라면 그자리에 reSize만큼 alloc
-	place(bp, reSize);
+	//아니라면 그자리에 size2만큼 alloc
+	place(bp, size2);
 
 	return bp;
 }
@@ -177,11 +177,10 @@ void mm_free(void *bp)
 void *mm_realloc(void *oldbp, size_t size)
 {
 	//원래 사이즈
-	size_t oldSize = GET_SIZE(HDRP(oldbp));
-	size_t reSize, blockNext, tmp, _size, sizeCopy;
+	size_t old_size = GET_SIZE(HDRP(oldbp));
+	size_t size2, next_block, copy_size;
 	//새 공간을 가리킬 포인터
 	void *newbp;
-
 
 	//만약 널포인터를 받았다면 그냥 그 사이즈 만큼 malloc
 	if (oldbp == NULL) {
@@ -196,28 +195,28 @@ void *mm_realloc(void *oldbp, size_t size)
 
 	//블락사이즈 보다 작거나 같으면 
 	if (size <= DSIZE) {
-		reSize = 2 * DSIZE;
+		size2 = 2 * DSIZE;
 	}
 	//블락사이즈보다 크다면 
 	else {
-		reSize = DSIZE * ((size + DSIZE + (DSIZE - 1)) / DSIZE);
+		size2 = DSIZE * ((size + DSIZE + (DSIZE - 1)) / DSIZE);
 	}
 
 	//만약 사이즈가 변경되지 않았다면 추가 작업필요 x 원래꺼 반환
-	if (reSize == oldSize) {
+	if (size2 == old_size) {
 		return oldbp;
 	}
 
 	//더 큰 공간으로 재 할당 하는 경우
-	else if (reSize > oldSize) {
+	else if (size2 > old_size) {
 		//오른쪽 블락과 합쳐서 해결가능한지 확인한다
-		blockNext = GET_SIZE(HDRP(NEXT_BLKP(oldbp)));
+		next_block = GET_SIZE(HDRP(NEXT_BLKP(oldbp)));
 		//옆에 블락이 할당이 안된 free 블락이고 재할당이 가능한 크기인 경우
 		if (!GET_ALLOC(HDRP(NEXT_BLKP(oldbp)))) {
-			if (blockNext + oldSize >= reSize) {
+			if (next_block + old_size >= size2) {
 				//가능하다면 옆의 블럭을 프리리스트에서 제외 시키고 헤더 풋터 정보 갱신
 				free_list_remove(NEXT_BLKP(oldbp));
-				put_head_foot(oldbp, oldSize + blockNext, 1);
+				put_head_foot(oldbp, old_size + next_block, 1);
 				return oldbp;
 			}
 		}
@@ -228,8 +227,8 @@ void *mm_realloc(void *oldbp, size_t size)
 		//새로운 공간에 재 할당
 		newbp = mm_malloc(size);
 		//정보 복사 : 복사할 크기= 원래크기에서 헤더랑풋터뺀만큼
-		sizeCopy = oldSize - DSIZE;
-		memcpy(newbp, oldbp, sizeCopy);
+		copy_size = old_size - DSIZE;
+		memcpy(newbp, oldbp, copy_size);
 		//원래 블락 free
 		mm_free(oldbp);
 		return newbp;
@@ -238,12 +237,12 @@ void *mm_realloc(void *oldbp, size_t size)
 	//더 적은 공간으로 재할당 하는 경우
 	else {
 		//줄어들어야 할 크기가 블락사이즈보다 크거나 같은경우
-		if (oldSize - reSize >= BLOCKSIZE) {
+		if (old_size - size2 >= BLOCKSIZE) {
 			//줄어든 사이즈로 정보 갱신
-			put_head_foot(oldbp, reSize, 1);
+			put_head_foot(oldbp, size2, 1);
 			//next 블럭 정보를 받아와서 헤더풋터 정보 갱신 후 free 리스트에 넣어준다.
 			newbp = NEXT_BLKP(oldbp);
-			put_head_foot(newbp, oldSize - reSize, 0);
+			put_head_foot(newbp, old_size - size2, 0);
 			put(newbp, 0);
 			free_list_insert(newbp);
 		}
@@ -289,16 +288,17 @@ static void *extend_heap(size_t words) {
 	char *bp;
 	size_t size;
 
-
+	/* Allocate an even number of words to maintain alignment */
 	size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
 	if ((long)(bp = mem_sbrk(size)) == -1)
 		return NULL;
 
+	/* Initialize free block header/footer and the epilogue header */
+	PUT(HDRP(bp), PACK(size, 0)); 			/* free block header */
+	PUT(FTRP(bp), PACK(size, 0)); 			/* free block footer */
+	PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); 	/* new epilogue header */
 
-	put_head_foot(bp, size, 0);
-	PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
-
-
+/* Coalesce if the previous block was free */
 	bp = coalesce(bp);
 
 	free_list_insert(bp);
@@ -314,25 +314,28 @@ static void *extend_heap(size_t words) {
  /* $begin mmplace */
  /* $begin mmplace-proto */
 static void place(void *bp, size_t asize) {
+	/* $end mmplace-proto */
+	size_t csize = GET_SIZE(HDRP(bp));
 
-	size_t hSize = GET_SIZE(HDRP(bp));
-
-	if ((hSize - asize) >= BLOCKSIZE) {
+	if ((csize - asize) >= BLOCKSIZE) {
 
 		free_list_remove(bp);
-		put_head_foot(bp, asize, 1);
+
+		PUT(HDRP(bp), PACK(asize, 1));
+		PUT(FTRP(bp), PACK(asize, 1));
 
 		bp = NEXT_BLKP(bp);
-		put_head_foot(bp, hSize - asize, 0);
+		PUT(HDRP(bp), PACK(csize - asize, 0));
+		PUT(FTRP(bp), PACK(csize - asize, 0));
 
-		put(bp, 0);
-
+		PUT(p_to_char(bp), 0);
+		PUT( pAtC(bp), 0);
 		free_list_insert(bp);
 	}
 	else {
 		free_list_remove(bp);
-		put_head_foot(bp, hSize, 1);
-
+		PUT(HDRP(bp), PACK(csize, 1));
+		PUT(FTRP(bp), PACK(csize, 1));
 	}
 }
 /* $end mmplace */
@@ -341,39 +344,41 @@ static void place(void *bp, size_t asize) {
  * find_fit - Find a fit for a block with asize bytes
  */
 static void *find_fit(size_t asize) {
-	void *class_p, *bp;
-	size_t b_size;
+
 	size_t tmp_s = asize;
-	int sizeC = 0;
-	int ch = 0, sum = 0;
-	while (asize > BLOCKSIZE && sizeC < ClassSize - 1) {
-		sizeC++;
-		ch += asize % 2;
+	int size_class = 0;
+	int sumr = 0;
+	while (asize > BLOCKSIZE && size_class < ClassSize - 1) {
+		size_class++;
+		sumr += asize % 2;
 		asize /= 2;
 	}
-	if (sizeC < ClassSize - 1 && ch > 0 && asize == BLOCKSIZE) {
-		sizeC++;
+	if (size_class < ClassSize - 1 && sumr > 0 && asize == BLOCKSIZE) {
+		size_class++;
 	}
+	
+	void *class_p, *bp;
+	size_t sizeB;
 
+	/* search from the rover to the end of list */
+	while (size_class < ClassSize) {
+		class_p = FreeBlocks + size_class;
 
-	while (sizeC < ClassSize) {
-		class_p = _free_listp + sizeC;
-
-
+		/* search from start of list to old rover */
 		if (GET(class_p) != 0) {
 			bp = (void *)GET(class_p);
 			while (bp != ((void *)0)) {
-				b_size = GET_SIZE(HDRP(bp));
-				if (tmp_s <= b_size) {
+				sizeB = GET_SIZE(HDRP(bp));
+				if (tmp_s <= sizeB) {
 					return bp;
 				}
 				bp = pAtC_GET(bp);
 			}
 		}
-		sizeC++;
+		size_class++;
 	}
 
-	return NULL;
+	return NULL; // no fit found
 }
 
 /*
@@ -384,12 +389,20 @@ static void *coalesce(void *bp) {
 	size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
 	size_t size = GET_SIZE(HDRP(bp));
 
-
-	if (prev_alloc && next_alloc) {
+	if (prev_alloc && next_alloc) {				/* Case 1 */
 		return bp;
 	}
 
-	else if (!prev_alloc && next_alloc) {
+	else if (prev_alloc && !next_alloc) {		/* Case 2 */
+
+		free_list_remove(NEXT_BLKP(bp));
+
+		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+		PUT(HDRP(bp), PACK(size, 0));
+		PUT(FTRP(bp), PACK(size, 0));
+	}
+
+	else if (!prev_alloc && next_alloc) {		/* Case 3 */
 
 		free_list_remove(PREV_BLKP(bp));
 
@@ -399,23 +412,14 @@ static void *coalesce(void *bp) {
 		bp = PREV_BLKP(bp);
 	}
 
-	else if (prev_alloc && !next_alloc) {
-
-		free_list_remove(NEXT_BLKP(bp));
-
-		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
-		put_head_foot(bp, size, 0);
-	}
-
-
-
-	else {
+	else {										/* Case 4 */
 
 		free_list_remove(PREV_BLKP(bp));
 		free_list_remove(NEXT_BLKP(bp));
 
 		size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
-		put_head_foot(bp, size, 0);
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+		PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
 		bp = PREV_BLKP(bp);
 	}
 
@@ -451,54 +455,54 @@ static void checkblock(void *bp)
 
 static void free_list_insert(void *bp) {
 	size_t size = GET_SIZE(HDRP(bp));
-	char **sizeC_ptr;
+	char **size_class_ptr;
 	unsigned int bp_val = (unsigned int)bp;
 
-	int sizeC = 0;
-	int ch = 0;
-	while (size > BLOCKSIZE && sizeC < ClassSize - 1) {
-		sizeC++;
-		ch += size % 2;
+	int size_class = 0;
+	int sumr = 0;
+	while (size > BLOCKSIZE && size_class < ClassSize - 1) {
+		size_class++;
+		sumr += size % 2;
 		size /= 2;
 	}
-	if (sizeC < ClassSize - 1 && ch > 0 && size == BLOCKSIZE) {
-		sizeC++;
+	if (size_class < ClassSize - 1 && sumr > 0 && size == BLOCKSIZE) {
+		size_class++;
 	}
+	
 
-
-	sizeC_ptr = _free_listp + sizeC;
-	if (GET(sizeC_ptr) == 0) {
-		PUT(sizeC_ptr, bp_val);
-		PUT(p_to_char(bp), (unsigned int)sizeC_ptr);
-		PUT(pAtC(bp), 0);
+	size_class_ptr = FreeBlocks + size_class;
+	if (GET(size_class_ptr) == 0) {
+		PUT(size_class_ptr, bp_val);
+		PUT(p_to_char(bp), (unsigned int)size_class_ptr);
+		PUT( pAtC(bp), 0);
 	}
 
 	else {
-		PUT(p_to_char(bp), (unsigned int)sizeC_ptr);
-		PUT(pAtC(bp), GET(sizeC_ptr));
-		PUT(p_to_char(GET(sizeC_ptr)), bp_val);
-		PUT(sizeC_ptr, bp_val);
+		PUT(p_to_char(bp), (unsigned int)size_class_ptr);
+		PUT( pAtC(bp), GET(size_class_ptr));
+		PUT(p_to_char(GET(size_class_ptr)), bp_val);
+		PUT(size_class_ptr, bp_val);
 	}
 
 }
 
 
 static void free_list_remove(void *bp) {
-	int pre;
+	int pre ;
 
 	unsigned int valP = (unsigned int)p_char_GET(bp);
-	unsigned int p = (unsigned int)_free_listp;
+	unsigned int p = (unsigned int)FreeBlocks;
 	unsigned int f = p + WSIZE * (ClassSize - 1);
 
 	if (valP > f || valP < p)
-		pre = 1;
+		pre= 1;
 	else if ((f - valP) % WSIZE)
-		pre = 1;
-	else pre = 0;
+		pre= 1;
+	else pre=0;
 	int suc = (pAtC_GET(bp) != (void *)0);
 
 	if (GET_ALLOC(HDRP(bp))) {
-
+		printf("calling free_list_remove on an allocated block\n");
 		return;
 	}
 
@@ -507,22 +511,21 @@ static void free_list_remove(void *bp) {
 		PUT(p_to_char(pAtC_GET(bp)), (unsigned int)p_char_GET(bp));
 	}
 
-
+	else if (!pre && !suc) {
+		PUT(p_char_GET(bp), (unsigned int)pAtC_GET(bp));
+	}
 
 	else if (pre && suc) {
 		PUT(pAtC(p_char_GET(bp)), (unsigned int)pAtC_GET(bp));
 		PUT(p_to_char(pAtC_GET(bp)), (unsigned int)p_char_GET(bp));
 	}
 
-	else if (!pre && !suc) {
-		PUT(p_char_GET(bp), (unsigned int)pAtC_GET(bp));
-	}
-
 	else {
 		PUT(pAtC(p_char_GET(bp)), 0);
 	}
 
-	put(bp, 0);
+	PUT(p_to_char(bp), 0);
+	PUT( pAtC(bp), 0);
 
 }
 
